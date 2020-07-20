08ded1c8b067b0d0677ba6c5914c30a596f48262
Shuai Chen <chenshuai92@hotmail.com>
2020-07-20 19:49:26 +0800

    add zuul

---
+++ gateway-zuul/src/main/java/com/ofloxacin/gateway/GatewayZuulApplication.java
@@ -0,0 +1,20 @@
+package com.ofloxacin.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author chenshuai
+ * @version 0.1
+ * @description
+ * @date 2020-07-20
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayZuulApplication.class, args);
+    }
+}
